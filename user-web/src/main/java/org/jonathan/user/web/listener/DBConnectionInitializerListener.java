0c66f8040f547f4e54acae69389efd2f60233a1c
Jonathan.Hu <NuoMark@163.com>
2021-03-14 08:53:16 +0800

    配置的补充
    对feature_firstweekf分支的补充

--- user-web/src/main/java/org/jonathan/user/web/listener/DBConnectionInitializerListener.java
+++ user-web/src/main/java/org/jonathan/user/web/listener/DBConnectionInitializerListener.java
@@ -6,6 +6,7 @@ import org.jonathan.user.sql.DBConnectionManager;
 import javax.naming.Context;
 import javax.naming.InitialContext;
 import javax.naming.NamingException;
+import javax.servlet.ServletContext;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 import javax.servlet.annotation.WebListener;
@@ -14,30 +15,36 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-@WebListener
 public class DBConnectionInitializerListener implements ServletContextListener {
+    private ServletContext servletContext;
 
     @Override
     public void contextInitialized(ServletContextEvent sce) {
-        System.out.println("Context initializing");
+        servletContext = sce.getServletContext();
+        servletContext.log("Context initializing");
         try {
             Connection connection = getConnectionByJndi();
             DBConnectionManager.getInstance().setConnection(connection);
         } catch (SQLException | NamingException e) {
-            e.printStackTrace();
+            servletContext.log(e.getMessage(), e);
         }
     }
 
     private Connection getConnectionByJndi() throws SQLException, NamingException {
         Context initContext = new InitialContext();
-        DataSource dataSource = (DataSource) initContext.lookup("java:comp/env/jdbc/UserPlatformDB");
-        System.out.println("data source: " + dataSource);
+        Context envContext = (Context) initContext.lookup("java:comp/env");
+        //依赖查找
+        DataSource dataSource = (DataSource) envContext.lookup("jdbc/UserPlatformDB");
+        if (initContext != null) {
+            servletContext.log("获取 JNDI 数据库连接成功");
+            servletContext.log("data source: " + dataSource);
+        }
         return dataSource.getConnection();
     }
 
     @Override
     public void contextDestroyed(ServletContextEvent sce) {
-        System.out.println("Context destroying");
+        servletContext.log("Context destroying");
         DBConnectionManager.getInstance().releaseConnection();
     }
 }
